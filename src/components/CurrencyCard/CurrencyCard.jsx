1cf473b1f04221f61ca3a3eaae2bfcbcfd6bc07c
Josh Bryson <josh@bryson.me.uk>
2020-07-11 20:28:59 +0100

    Dynamic currency symbol formatting added

--- src/components/CurrencyCard/CurrencyCard.jsx
+++ src/components/CurrencyCard/CurrencyCard.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './CurrencyCard.scss';
 
 const CurrencyCard = ({ handleChange, inputChange, inputValue, objectKey, optionValue, value }) => {
-  console.log('KEY', objectKey)
+
+  const currencySymbolFormatter = new Intl.NumberFormat(undefined, {
+    style: 'currency',
+    currency: `${value}`
+  })
+    
   return (
     objectKey ? (
       <div className="currency-card">
-        <input className="currency-card-input" value={inputValue} onChange={inputChange} />
+        <input className="currency-card-input" value={currencySymbolFormatter.format(inputValue)} onChange={inputChange} />
         <select className="currency-card-select" value={value} onChange={handleChange}>
           <option value={`${optionValue}`}>{optionValue}</option>
           {Object.keys(objectKey.rates).map((rate, key) => (
